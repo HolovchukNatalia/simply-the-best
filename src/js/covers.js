826e2323cbdfc4d844e08b10529a9134ea8c06f8
Falka <valentyna.stashenko@gmail.com>
2025-03-08 21:35:50 +0200

    markup and desktop css

--- src/js/covers.js
+++
@@ -1,7 +0,0 @@
-import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
-
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css';
-
-import axios from 'axios';
