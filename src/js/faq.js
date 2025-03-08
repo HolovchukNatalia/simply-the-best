95ed2dee7c7f802681bd43c411b7ef3f9ab4c880
Falka <valentyna.stashenko@gmail.com>
2025-03-08 22:19:36 +0200

    arrow size fix, tablet text measures, multiple open in accordion

--- src/js/faq.js
+++ src/js/faq.js
@@ -6,4 +6,7 @@ import 'accordion-js/dist/accordion.min.css';
 
 // import axios from 'axios';
 
-new Accordion('.accordion-container', { triggerClass: 'faq-trigger' });
+new Accordion('.accordion-container', {
+  triggerClass: 'faq-trigger',
+  showMultiple: true,
+});
