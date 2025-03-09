fd9da64c7158c9a258cb84d06aaa362f95c85ff8
Katya Gladka <katya.gladkaya.29@gmail.com>
2025-03-09 23:18:08 +0200

    header-1

--- src/js/header.js
+++ src/js/header.js
@@ -5,3 +5,33 @@ import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
 
 import axios from 'axios';
+
+document.getElementById('menu-btn').addEventListener('click', function () {
+document.querySelector('.header-nav').classList.remove('visually-hidden');
+document.addEventListener('click', function (event) {
+if (event.target !== document.getElementById('menu-btn')) {
+document.querySelector('.header-nav').classList.add('visually-hidden');
+}
+});
+});
+
+const burger = document.getElementById('burger');
+const mobileMenu = document.getElementById('mobile-menu');
+const closeMenu = document.getElementById('close-menu');
+
+burger.addEventListener('click', () => {
+mobileMenu.classList.add('is-opened');
+document.body.style.overflow = 'hidden';
+});
+
+closeMenu.addEventListener('click', () => {
+mobileMenu.classList.remove('is-opened');
+document.body.style.overflow = 'auto';
+});
+
+document.querySelectorAll('.mobile-menu-list a').forEach(link => {
+link.addEventListener('click', () => {
+mobileMenu.classList.remove('is-opened');
+document.body.style.overflow = 'auto';
+});
+});
