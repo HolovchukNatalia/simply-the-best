23f784848d75e9a2c50ca6f1a551c4c32d5f1457
Anhelina Dobra <dobraangelina@gmail.com>
2025-03-09 10:20:03 +0100

    fix elements sizes

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,7 +1,5 @@
-import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
 
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css';
 
-import axios from 'axios';
+
+
+
