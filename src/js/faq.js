8387003553670f3d221114cab4d5d89b33291563
Natalia <holovshyk@gmail.com>
2025-03-09 12:14:34 +0200

    fix faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,11 +1,6 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-// import Swiper from 'swiper';
-// import 'swiper/swiper-bundle.css';
-
-// import axios from 'axios';
-
 new Accordion('.accordion-container', {
   triggerClass: 'faq-trigger',
   showMultiple: true,
