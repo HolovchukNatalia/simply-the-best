61f0cd6083db2d320bf59cdf006e3ef941294318
holovchuchka <120129831+HolovchukNatalia@users.noreply.github.com>
2025-03-09 12:09:23 +0200

    Revert "FAQ section"

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,12 +1,7 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
 
-// import Swiper from 'swiper';
-// import 'swiper/swiper-bundle.css';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
 
-// import axios from 'axios';
-
-new Accordion('.accordion-container', {
-  triggerClass: 'faq-trigger',
-  showMultiple: true,
-});
+import axios from 'axios';
