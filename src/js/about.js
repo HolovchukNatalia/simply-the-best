b63f3f58fa6eeab254cf684d8df603f7c62b4e8a
Falka <valentyna.stashenko@gmail.com>
2025-03-12 18:35:00 +0200

    top + accordion

--- src/js/about.js
+++ src/js/about.js
@@ -1,44 +1,37 @@
-import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
 import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+import { Navigation, Keyboard } from 'swiper/modules';
 
-import axios from 'axios';
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const items = document.querySelectorAll(".about-me-item");
-
-    items.forEach(item => {
-        const toggleContainer = item.querySelector(".toggle-container");
-        const content = item.querySelector(".list-content");
-        const icon = item.querySelector(".arrow-icon");
-
-        toggleContainer.addEventListener("click", function () {
-            content.classList.toggle("active");
-            toggleContainer.classList.toggle("active");
-        });
-    });
+new Accordion('.list-about-me', {
+  triggerClass: 'about-trigger',
+  showMultiple: true,
+  openOnInit: [0],
 });
 
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const wordList = document.querySelectorAll(".word-list .teach");
-    const arrow = document.querySelector(".right-arrow-icon");
-
-    arrow.addEventListener("click", function () {
-        let texts = Array.from(wordList).map(el => el.textContent); // Отримуємо всі тексти
-        texts.unshift(texts.pop()); // Переміщуємо останній елемент на перше місце
-
-        wordList.forEach((el, index) => {
-            el.style.opacity = "0"; // Плавне зникнення тексту
-
-            setTimeout(() => {
-                el.textContent = texts[index]; // Зміна тексту
-                el.style.opacity = "1"; // Плавне повернення тексту
-            }, 300); // Час зміни тексту (має співпадати з CSS)
-        });
-    });
-});
+// const swiper = new Swiper('.word-list', {
+//   modules: [Navigation, Keyboard],
+//   slidesPerView: 2,
+//   spaceBetween: 0,
+//   loop: true,
+//   navigation: {
+//     nextEl: '.word-button-next',
+//   },
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: true,
+//   },
+//   grabCursor: true,
+//   //   on: {
+//   //     init: function () {
+//   //       this.navigation.update();
+//   //     },
+//   //     slideChange: function () {
+//   //       this.navigation.update();
+//   //     },
+//   //   },
+// });
