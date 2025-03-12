06809d53da293dca224f345005cba826fcd2b221
Pivtorak Artem <mulbrry.digital@gmail.com>
2025-03-12 11:49:52 +0200

    error js

--- src/js/about.js
+++ src/js/about.js
@@ -7,38 +7,21 @@ import 'swiper/swiper-bundle.css';
 import axios from 'axios';
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const items = document.querySelectorAll(".about-me-item");
-
-    items.forEach(item => {
-        const toggleContainer = item.querySelector(".toggle-container");
-        const content = item.querySelector(".list-content");
-        const icon = item.querySelector(".arrow-icon");
-
-        toggleContainer.addEventListener("click", function () {
-            content.classList.toggle("active");
-            toggleContainer.classList.toggle("active");
+        document.addEventListener("DOMContentLoaded", function() {
+            new Accordion('.accordion-container', {
+                duration: 400,
+                showMultiple: true
+            });
         });
+
+    document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper(".word-list", {
+        loop: true,
+        slidesPerView: 1,
+        allowTouchMove: false,
     });
-});
 
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const wordList = document.querySelectorAll(".word-list .teach");
-    const arrow = document.querySelector(".right-arrow-icon");
-
-    arrow.addEventListener("click", function () {
-        let texts = Array.from(wordList).map(el => el.textContent); // Отримуємо всі тексти
-        texts.unshift(texts.pop()); // Переміщуємо останній елемент на перше місце
-
-        wordList.forEach((el, index) => {
-            el.style.opacity = "0"; // Плавне зникнення тексту
-
-            setTimeout(() => {
-                el.textContent = texts[index]; // Зміна тексту
-                el.style.opacity = "1"; // Плавне повернення тексту
-            }, 300); // Час зміни тексту (має співпадати з CSS)
-        });
+    document.querySelector(".arrow-right").addEventListener("click", function () {
+        swiper.slideNext();
     });
 });
